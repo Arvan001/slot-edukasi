300ce03fcef6652f0927d6af251e34f28abf0a38
Arvan001 <hermanudin180@gmail.com>
2025-07-25 12:11:51 +0700

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -3,14 +3,14 @@ CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL,
-    saldo INTEGER DEFAULT 0
+    saldo INTEGER NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS settings (
-    id INTEGER PRIMARY KEY CHECK (id = 1),
-    mode_otomatis BOOLEAN DEFAULT 0,
-    persentase_menang INTEGER DEFAULT 0,
-    min_menang INTEGER DEFAULT 50000,
+    id INTEGER PRIMARY KEY,
+    mode_otomatis INTEGER DEFAULT 1,
+    persentase_menang INTEGER DEFAULT 30,
+    min_menang INTEGER DEFAULT 10000,
     max_menang INTEGER DEFAULT 100000,
     default_saldo INTEGER DEFAULT 100000
 );
